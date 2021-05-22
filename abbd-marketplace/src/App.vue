49155c8b48b5925dc5b92c0e88c0c097d25bce85
sebastienbouejob <68823990+sebastienbouejob@users.noreply.github.com>
2021-05-22 20:10:10 +0200

    (feat) Mise en place du projet

---
+++ abbd-marketplace/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'App',
+    components: {
+    },
+    data() {
+      return {}
+    },
+    methods: {},
+
+  }
+</script>
+
+
+<style lang="css" scoped>
+
+  #app {
+    display: flex;
+    flex-direction: row;
+  }
+
+
+</style>
